6e54f919cac1b24539866f692383204cf8a7b587
JIA YEE <jiayeerawr@gmail.com>
2018-01-11 03:15:51 +0800

    Change query again
    Signed-off-by: JIA YEE <jiayeerawr@gmail.com>

--- app.py
+++ app.py
@@ -60,7 +60,7 @@ def get_today_menu():
     print(chat_id)
     print(message)
     if message == '/start':
-        menu_items = db.session.query(MenuItem).filter(MenuItem.date == datetime.now().strftime('%Y-%m-%d'))
+        menu_items = MenuItem.query.filter_by(date=datetime.now().strftime('%Y-%m-%d'))
         print(menu_items)
         pretty_menu_items = get_pretty(menu_items)
         print(pretty_menu_items)
