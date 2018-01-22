010ce351944508f4069d8b12e16c8783d16f6f09
JIA YEE <jiayeerawr@gmail.com>
2018-01-22 19:40:28 +0800

    Add print statements for the concerned values
    Signed-off-by: JIA YEE <jiayeerawr@gmail.com>

--- validate.py
+++ validate.py
@@ -6,13 +6,12 @@ csv_file_path = sys.argv[1]
 with open(csv_file_path, 'r') as f:
     reader = csv.reader(f)
     columns = list(filter(None, next(reader)))
-    # Assert number of columns
-    # Assert column names
+    print(columns)
     last_column_index = len(columns) - 1
     for csv_row in reader:
         values = csv_row[:last_column_index]
-        # Assert date time format
-        dishes = csv_row[last_column_index:]
-        # Assert no comma in each dish
-        # This is a possible indication that multiple dishes will be read as one dish
+        date_value = values[0]
+        print(date_value)
+        dishes = list(filter(None, csv_row[last_column_index:]))
+        print(dishes)
 
